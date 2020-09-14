d62964d88b09648da371955021bed0d89ddd872d
Wilder Rincon <1588@holbertonschool.com>
2020-09-14 11:50:20 -0500

    Adding file exercise 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Python script to export data in the JSON format"""
+
+import json
+import requests
+
+if __name__ == '__main__':
+    users = requests.get("https://jsonplaceholder.typicode.com/users",
+                         verify=False).json()
+    userdict = {}
+    usernamedict = {}
+    for user in users:
+        uid = user.get("id")
+        userdict[uid] = []
+        usernamedict[uid] = user.get("username")
+    to_do = requests.get("https://jsonplaceholder.typicode.com/todos",
+                        verify=False).json()
+    for task in to_do:
+        taskdict = {}
+        uid = task.get("userId")
+        taskdict["task"] = task.get('title')
+        taskdict["completed"] = task.get('completed')
+        taskdict["username"] = usernamedict.get(uid)
+        userdict.get(uid).append(taskdict)
+    with open("todo_all_employees.json", 'w') as jsonfile:
+        json.dump(userdict, jsonfile)
