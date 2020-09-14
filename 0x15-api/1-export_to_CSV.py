65e2449e6f5278e8f70bc26b37ac609572320bd1
Wilder Rincon <1588@holbertonschool.com>
2020-09-14 11:33:56 -0500

    Adding file exercise 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,11 +9,11 @@ if __name__ == '__main__':
     userId = argv[1]
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
                         format(userId), verify=False).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+    to_do = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
                         format(userId), verify=False).json()
-    with open("{}.csv".format(userId), 'w', newline='') as csvfile:
+    with open("{}.csv".format(userId), mode='w', newline='') as csvfile:
         taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for task in todo:
+        for task in to_do:
             taskwriter.writerow([int(userId), user.get('username'),
                                  task.get('completed'),
                                  task.get('title')])
