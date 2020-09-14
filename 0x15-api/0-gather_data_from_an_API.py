7f8161d45e602ecd53a1d42f94288db4ca54db70
Wilder Rincon <1588@holbertonschool.com>
2020-09-14 17:10:22 -0500

    Adding file exercise 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,24 +4,41 @@
 """
 
 
+import requests
+import sys
+
+base_url = 'https://jsonplaceholder.typicode.com/'
+
+
+def do_request():
+    '''Performs request'''
+    if len(sys.argv) < 2:
+        return print('USAGE:', __file__, '<employee id>')
+    eid = sys.argv[1]
+    try:
+        _eid = int(sys.argv[1])
+    except ValueError:
+        return print('Employee id must be an integer')
+
+    response = requests.get(base_url + 'users/' + eid)
+    if response.status_code == 404:
+        return print('User id not found')
+    elif response.status_code != 200:
+        return print('Error: status_code:', response.status_code)
+    user = response.json()
+
+    response = requests.get(base_url + 'todos/')
+    if response.status_code != 200:
+        return print('Error: status_code:', response.status_code)
+    todos = response.json()
+
+    user_todos = [todo for todo in todos
+                  if todo.get('userId') == user.get('id')]
+    completed = [todo for todo in user_todos if todo.get('completed')]
+    print('Employee', user.get('name'),
+          'is done with tasks({}/{}):'.
+          format(len(completed), len(user_todos)))
+    [print('\t', todo.get('title')) for todo in completed]
+
 if __name__ == '__main__':
-    import requests
-    from sys import argv
-
-    API_URL = "https://jsonplaceholder.typicode.com/"
-    people = requests.get(API_URL + "users/" + argv[1])
-    to_do = requests.get(API_URL + "todos?userId=" + argv[1])
-    name = people.json().get("name")
-    tasks = len(to_do.json())
-    done = 0
-
-    for task in to_do.json():
-        if task.get("completed"):
-            done += 1
-
-    print("Employee {} is done with tasks({}/{}):".format(name, done, tasks))
-
-    for task in to_do.json():
-        if task.get("completed"):
-            print("\t ", end="")
-            print(task.get("title"))
+    do_request()
