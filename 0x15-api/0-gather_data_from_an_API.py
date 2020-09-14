05a8265858294982d270049b73a9b481512029c7
Wilder Rincon <1588@holbertonschool.com>
2020-09-14 16:54:43 -0500

    Adding file exercise 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,19 +3,25 @@
     Given employee ID, returns information about his/her TODO list progress.
 """
 
-import requests
-from sys import argv
 
 if __name__ == '__main__':
-    userId = argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(userId), verify=False).json()
-    to_do = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(userId), verify=False).json()
-    completed_tasks = []
-    for task in to_do:
-        if task.get('completed') is True:
-            completed_tasks.append(task.get('title'))
-    print("Employee {} is done with tasks({}/{}):".
-          format(user.get('name'), len(completed_tasks), len(to_do)))
-    print("\n".join("\t {}".format(task) for task in completed_tasks))
+    import requests
+    from sys import argv
+
+    API_URL = "https://jsonplaceholder.typicode.com/"
+    people = requests.get(API_URL + "users/" + argv[1])
+    to_do = requests.get(API_URL + "todos?userId=" + argv[1])
+    name = people.json().get("name")
+    tasks = len(to_do.json())
+    done = 0
+
+    for task in to_do.json():
+        if task.get("completed"):
+            done += 1
+
+    print("Employee {} is done with tasks({}/{}):".format(name, done, tasks))
+
+    for task in to_do.json():
+        if task.get("completed"):
+            print("\t ", end="")
+            print(task.get("title"))
